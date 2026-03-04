dee9e705f30e39b50ba7f2daae6470b7ea46b337
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-05 01:57:20 +0530

    change: delete backend files

--- backend/src/router/mailRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { sendMailController } from '../Controller/emailController.js';
-
-const router = express.Router()
-
-router.post('/sendMail', sendMailController)
-
-export default router
